ec49109bb26015a82e3a755cd1c6630fdb0ac03e
Kent Fujii <kent.where.the.light.is@gmail.com>
2016-07-03 08:39:56 +0900

    Finish: chapter6

---
+++ chapter6/lessons/circle.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def create_circle():
+    circle = plt.Circle((0, 0), radius = 0.5)
+    return circle
+def show_shape(patch):
+    ax = plt.gca()
+    ax.add_patch(patch)
+    plt.axis('scaled')
+    plt.show()
+
+c = create_circle()
+show_shape(c)
